f25aac16aee048aa7c88f66f735a90393f325d3d
serraguti <pacoserranox@gmail.com>
2022-12-09 16:48:23 +0100

    COMMIT

---
+++ python38conexionsql.py
@@ -0,0 +1,22 @@
+import pyodbc
+print("Primera consulta SQL Server")
+# servidor="LOCALHOST\SQLEXPRESS"
+# bbdd="HOSPITAL"
+# usuario="SA"
+# password=""
+servidor = "sqleoi.database.windows.net"
+bbdd = "AZUREALUMNOS"
+usuario = "adminsql"
+password = "Admin123"
+#CADENA CONEXION CON SEGURIDAD SQL SERVER (REMOTO)
+cadenaconexion=("DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + servidor
++ "; DATABASE=" + bbdd + "; UID=" + usuario + "; PWD=" + password)
+try:
+    print("Intentando conectar...")
+    conexion = pyodbc.connect(cadenaconexion)
+    print("Conectado!!!")
+except pyodbc.InterfaceError:
+    print("Buff, esto no va bien")
+finally:
+    conexion.close()
+print("Fin de programa")
