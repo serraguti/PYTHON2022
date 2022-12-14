ad1cea418b5a0ce198bf7895258c7a25fb5e0bad
serraguti <pacoserranox@gmail.com>
2022-12-14 19:22:05 +0100

    commit

--- conexion44enfermos.py
+++ conexion44enfermos.py
@@ -1,5 +1,5 @@
 import pyodbc
-servidor = "LOCALHOST\LOCALHOST"
+servidor = "LOCALHOST"
 bbdd = "HOSPITAL"
 usuario = "SA"
 password = ""
--- main44conexionenfermos.py
+++ main44conexionenfermos.py
@@ -4,10 +4,14 @@ from conexion44enfermos import ConexionEnfermos
 
 # CREAMOS UN OBJETO DE LA CLASE CONEXIONENFERMOS
 connection = ConexionEnfermos()
-print("Escriba una inscripción para eliminar")
+print("Escriba una inscripción")
 inscripcion = input()
+print("Escriba el nuevo apellido del enfermo")
+apellido = input()
+respuesta = connection.modificarEnfermo(apellido, inscripcion)
+print("Apellidos modificados: " + str(respuesta))
 # LA CLASE CONEXION NOS DEVUELVE EL NUMERO DE ENFERMOS
 # ELIMINADOS
-respuesta = connection.eliminarEnfermo(inscripcion)
-print("Enfermos eliminados: " + str(respuesta))
+# respuesta = connection.eliminarEnfermo(inscripcion)
+# print("Enfermos eliminados: " + str(respuesta))
 print("Fin de programa")
