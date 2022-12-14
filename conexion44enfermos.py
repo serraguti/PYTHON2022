634f07b468bd2ddb1e195c8a6ec59506781aac30
serraguti <pacoserranox@gmail.com>
2022-12-14 18:37:55 +0100

    commit

---
+++ conexion44enfermos.py
@@ -0,0 +1,40 @@
+import pyodbc
+servidor = "LOCALHOST\SQLEXPRESS"
+bbdd = "HOSPITAL"
+usuario = "SA"
+password = ""
+
+class ConexionEnfermos:
+    # EN EL INICIO DE LA CLASE DEBEMOS CREAR
+    # UN OBJETO CONEXION PARA UTILIZARLO EN TODOS
+    # LOS METODOS (CONSTRUCTOR)
+    def __init__(self):
+        # self ES LA CLASE EN LA QUE ESTAMOS TRABAJANDO
+        # Y NOS VAMOS A CREAR UNA PROPIEDAD PARA 
+        # TENER LA CONEXION
+        connectionString = "DRIVER={ODBC Driver 17 for SQL Server}; SERVER="
+        + servidor + "; DATABASE=" + bbdd + "; UID=" + usuario + "; PWD=" + password
+        # CREAMOS UNA PROPIEDAD LLAMADA conexion
+        self.conexion = pyodbc.connect(connectionString)
+    # TENDREMOS UN METODO PARA ELIMINAR UN ENFERMO
+    # POR SU INSCRIPCION
+    def eliminarEnfermo(self, inscripcion):
+        # CREAR CURSOR, EJECUTAR Y CERRAR 
+        cursor = self.conexion.cursor()
+        sql = "DELETE FROM ENFERMO WHERE INSCRIPCION=?"
+        cursor.execute(sql, (inscripcion))
+        eliminados = cursor.rowcount
+        cursor.commit()
+        cursor.close()
+        return eliminados
+    
+    def modificarEnfermo(self, apellido, inscripcion):
+        # CREAR CURSOR, EJECUTAR Y CERRAR
+        cursor = self.conexion.cursor()
+        sql = "UPDATE ENFERMO SET APELLIDO=? WHERE INSCRIPCION=?"
+        cursor.execute(sql, (apellido, inscripcion))
+        modificados = cursor.rowcount
+        cursor.commit()
+        cursor.close()
+        return modificados
+
